3c0240b1f1e0f77efa677f6e588d03f15b6e1c12
ratlord <bennett.daniel@gmail.com>
2023-03-10 16:33:18 -0500

    event, attribute, class&style binding

--- src/app/attribute-binding/attribute-binding.component.ts
+++ src/app/attribute-binding/attribute-binding.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./attribute-binding.component.scss']
 })
 export class AttributeBindingComponent {
-
+  draggableBoolean: boolean = true;
 }
--- src/app/class-style-binding/class-style-binding.component.ts
+++ src/app/class-style-binding/class-style-binding.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./class-style-binding.component.scss']
 })
 export class ClassStyleBindingComponent {
+  onSale: boolean = true;
+  classExpression: string = 'big-font green-bg big-font-weight';
+  arrayExpression: string[] = ['dog', 'bone'];
+  objectExpression: {} = {"cemetary-bg": true, "frail-size": true, "white-text": true, "booyah-padding":true }
+
+
+  selectedBgColor: string = 'salmon';
+  styleExpression: string = 'width: 80%; height: 100px; background-Color: cornflowerblue; border: 2px dashed black; font-size: 1.5rem; color: grey'
 
 }
