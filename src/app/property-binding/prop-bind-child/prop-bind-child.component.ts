0ca1434574bb62a7da61510d5e2b2b51d8f00fa3
ratlord <bennett.daniel@gmail.com>
2023-03-11 07:48:33 -0500

    @Output added

--- src/app/property-binding/prop-bind-child/prop-bind-child.component.ts
+++ src/app/property-binding/prop-bind-child/prop-bind-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-prop-bind-child',
@@ -6,9 +6,17 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./prop-bind-child.component.scss']
 })
 export class PropBindChildComponent {
+  // Input from parent
   @Input() parentString: string = '';
   @Input() parentVar: any = '';
   @Input() parentObj: {} = {};
-  
+
+
+  // Output to parent
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  addNewItem(value: string) {
+    this.newItemEvent.emit(value);
+  }
 
 }
--- src/app/property-binding/property-binding.component.ts
+++ src/app/property-binding/property-binding.component.ts
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./property-binding.component.scss']
 })
 export class PropertyBindingComponent {
+  // Input to child
   varToChild: number = 108;
   objToChild: {} = { name:"Greg", weight:186 }
+
+
+  // Receiving output from child and pushing to list for display
+  items = ['item1', 'item2', 'item3', 'item4'];
+
+  addItemFromChild(newItem: string) {
+    this.items.push(newItem);
+    console.log(this.items);
+
+  }
+
 }
